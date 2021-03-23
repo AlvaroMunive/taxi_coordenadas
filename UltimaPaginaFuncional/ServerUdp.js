23654332307b3f242a8e9da376d459d4384f25a0
badiaz <badiaz@uninorte.edu.co>
2021-03-23 16:08:51 -0500

    prueba5

--- UltimaPaginaFuncional/ServerUdp.js
+++ UltimaPaginaFuncional/ServerUdp.js
@@ -12,7 +12,7 @@ app.use(express.static(__dirname));
 const {pool,Client}= require("pg")
 // Parametros para para la conexion con postgresql
 const connectionString="postgressql://Brayan:tiotaxi22@basededatostaxi.csgckedzjvw7.us-east-2.rds.amazonaws.com:5432/taxi_coordenadas"
-// Entanblar conexion con postgresql
+// Entablar conexion con postgresql
 const client = new Client({
   connectionString:connectionString
 })
@@ -46,7 +46,7 @@ socket.on('message', (msg, rinfo) => {
     time=data[2]
 
 // Insertar dato entrante a la base de datos
-    client.query('INSERT INTO public.taxi_coordenadas("Latitud", "Longitud", "Time")VALUES ('+latitud+','+longitud+','+time+');', (err,res)=>{
+    client.query('INSERT INTO public.T_coordenadas("Latitud", "Longitud", "Time")VALUES ('+latitud+','+longitud+','+time+');', (err,res)=>{
     console.log(err,res);
   
     })
