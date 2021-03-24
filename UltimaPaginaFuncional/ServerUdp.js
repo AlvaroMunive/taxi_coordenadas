4e36c648d3e0f57613432042f057bff5c5d7cd61
styv <kevinstyvenmartinez@hotmail.com>
2021-03-23 19:44:24 -0500

    Prueba1

--- UltimaPaginaFuncional/ServerUdp.js
+++ UltimaPaginaFuncional/ServerUdp.js
@@ -1,4 +1,4 @@
-
+require('events').EventEmitter.prototype._maxListeners = 0;
 var express = require('express');
 const dgram = require('dgram');
 var mysql = require('mysql');
@@ -8,6 +8,17 @@ var app = express();
 var server = require('http').Server(app);
 const socket = dgram.createSocket('udp4');
 app.use(express.static(__dirname));
+// Module postgresql
+const {pool,Client}= require("pg")
+// Parametros para para la conexion con postgresql
+const connectionString="postgressql://Alvaro:azereje12@serverbase.cigz349a2wix.us-east-2.rds.amazonaws.com:5432/serverbase0"
+// Entanblar conexion con postgresql
+const client = new Client({
+  connectionString:connectionString
+})
+
+client.connect()
+
 
 var data
 var longitud
@@ -29,12 +40,19 @@ socket.on('message', (msg, rinfo) => {
     msg = msg.split(');').join('')
     data=msg.split(",")
     longitud=data[0];
-    
+
     latitud= data[1];
-    
+
     time=data[2]
-    
-    
+
+// Insertar dato entrante a la base de datos
+    client.query('INSERT INTO public.taxi_coordenadas("Latitud", "Longitud", "Time")VALUES ('+latitud+','+longitud+','+time+');', (err,res)=>{
+    console.log(err,res);
+
+    })
+
+
+
 
 
 });
@@ -44,8 +62,8 @@ setInterval(function(){
     data=JSON.stringify(data);
     data=('{\"lat\":'+latitud+',\"lon\":'+longitud+',\"tim\":'+time+'}')
     data=data.split("'").join('"')
-    
-    
+
+
    fs.writeFile("lastData.txt",data,function (err) {
      if (err) throw err;
  });
