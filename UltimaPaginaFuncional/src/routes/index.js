3488aa19852a199b4bb168b25e4c4c8326683e72
alvaro_munive <calaa@uninote.edu.co>
2021-04-07 09:30:12 -0500

    Pagina V2.1

---
+++ UltimaPaginaFuncional/src/routes/index.js
@@ -0,0 +1,16 @@
+const { Router } = require("express")
+const router = Router();
+const client = require('./../database');
+const path = require('path')
+
+//routes
+
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "../view/index.html"))
+})
+
+router.get('/ubicar', (req, res) => {
+    res.sendFile(path.join(__dirname, "../view/ubicar.html"))
+})
+
+module.exports = router
