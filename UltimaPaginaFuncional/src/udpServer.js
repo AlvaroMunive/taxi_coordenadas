3c1400f444bf6339c4b3731f56ef0b6d9b3d5c0a
styv <kevinstyvenmartinez@hotmail.com>
2021-04-07 16:06:43 -0500

    Prueba1

---
+++ UltimaPaginaFuncional/src/udpServer.js
@@ -0,0 +1,46 @@
+require('events').EventEmitter.prototype._maxListeners = 0;
+const dgram = require('dgram');
+const socket = dgram.createSocket('udp4');
+
+const { pool, Client } = require("pg");
+const connectionString =
+  "postgressql://Alvaro:azereje12@serverbase.cigz349a2wix.us-east-2.rds.amazonaws.com:5432/serverbase0";
+const client = new Client({
+connectionString: connectionString,
+});
+
+client.connect(function(err) {
+    if(err){
+        console.log(err)
+    }
+});
+
+
+var data
+
+
+socket.on('error', (err) => {
+    console.log(`server error:\n${err.stack}`);
+    socket.close();
+});
+
+
+socket.on('message', (msg, rinfo) => {
+
+    msg = msg.toString()
+    msg = msg.split('INSERT INTO `taxis1_coordenadas` (`Longitud`, `Latitud`, `Tiempo`) VALUES (').join('')
+    msg = msg.split(');').join('')
+    data = msg.split(",")
+    
+
+    // Insertar dato entrante a la base de datos
+    client.query('INSERT INTO public.taxi_coordenadas("Latitud", "Longitud", "Time")VALUES (' + data[1] + ',' + data[0] + ',' + data[2] + ');', (err, res) => {
+        if(err){
+            console.log(err);    
+        }
+        
+
+    });
+
+});
+socket.bind(3020);
