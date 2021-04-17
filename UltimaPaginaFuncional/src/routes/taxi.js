eb09dbc8500de30c7489a7871167268d1621bbc4
badiaz <badiaz@uninorte.edu.co>
2021-04-17 11:59:12 -0500

    prueba 17abril

--- UltimaPaginaFuncional/src/routes/taxi.js
+++ UltimaPaginaFuncional/src/routes/taxi.js
@@ -5,16 +5,33 @@ const client = require("./../database");
 
 //routes
 
-router.get("/api", (req, res) => {
-  //Obtener el ultimo dato de la base de datos
-  client.query(`SELECT "Latitud", "Longitud", "Time" FROM public.taxi_coordenadas ORDER BY id DESC LIMIT 1;`, (err, row, field) => {
-    if (!err) {
-      res.json(row);
-    } else {
-      console.log(err);
-    }
+router.get("/api/:id", (req, res) => {
+  var { id } = req.params;
 
-  })
+  if (id == "1") {
+    //Obtener el ultimo dato de la base de datos
+    client.query(`SELECT "Latitud", "Longitud", "Time" FROM public.taxi_coordenadas ORDER BY id DESC LIMIT 1;`, (err, row, field) => {
+      if (!err) {
+        res.json(row.rows);
+      } else {
+        console.log(err);
+      }
+
+    })
+
+  } else if (id == "2") {
+    //Obtener el ultimo dato de la base de datos
+    client.query(`SELECT "Latitud", "Longitud", "Time" FROM public.taxi_coordenadas2 ORDER BY id DESC LIMIT 1;`, (err, row, field) => {
+      if (!err) {
+        res.json(row.rows);
+      } else {
+        console.log(err);
+      }
+
+    })
+
+  }
+
 
 });
 
@@ -25,16 +42,31 @@ router.get("/api/polyline/:id", (req, res) => {
   const start = id[0].split("T").join(" ");
   const end_ = id[1].split("T").join(" ");
   //Obtener los datos en un rango especificador por id
-  client.query(
-    `SELECT "Latitud","Longitud","Time" FROM public.taxi_coordenadas WHERE "Time" >=\'${start}\' AND "Time" <=\'${end_}\';`,
-    (err, rows, fields) => {
-      if (!err) {
-        res.json(rows);
-      } else {
-        console.log(err);
+  if (id[2] == "1") {
+    client.query(
+      `SELECT "Latitud","Longitud","Time" FROM public.taxi_coordenadas WHERE "Time" >=\'${start}\' AND "Time" <=\'${end_}\';`,
+      (err, rows, fields) => {
+        if (!err) {
+          res.json(rows);
+        } else {
+          console.log(err);
+        }
       }
-    }
-  );
+    );
+  } else if (id[2] == "2") {
+    client.query(
+      `SELECT "Latitud","Longitud","Time" FROM public.taxi_coordenadas2 WHERE "Time" >=\'${start}\' AND "Time" <=\'${end_}\';`,
+      (err, rows, fields) => {
+        if (!err) {
+          res.json(rows);
+        } else {
+          console.log(err);
+        }
+      }
+    );
+
+  }
+
 });
 
 
