1f27df1a567cec175fb4c322703793c6156420b0
badiaz <badiaz@uninorte.edu.co>
2021-04-29 13:48:18 -0500

    prueba 29abril

--- UltimaPaginaFuncional/src/routes/index.js
+++ UltimaPaginaFuncional/src/routes/index.js
@@ -1,16 +1,19 @@
-const {Router }= require("express")
-const router= Router();
-const client= require('./../database');
-const path=require('path')
+const { Router } = require("express")
+const router = Router();
+const client = require('./../database');
+const path = require('path')
 
 //routes
 //Ruta raiz
-router.get('/',(req,res)=>{
-    res.sendFile(path.join(__dirname,"../view/index.html"))
-    })
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "../view/index.html"))
+})
 //Ruta para obtener historial
-router.get('/ubicar',(req,res)=>{
-    res.sendFile(path.join(__dirname,"../view/ubicar.html"))
-    })  
+router.get('/ubicar', (req, res) => {
+    res.sendFile(path.join(__dirname, "../view/ubicar.html"))
+})
+router.get('/ubicar/click_map', (req, res) => {
+    res.sendFile(path.join(__dirname, "../view/click_map.html"))
+})
 //Exportar rutas
-module.exports=router
+module.exports = router
